8e3f3c2798488599650242dcaba096d65e77c6a6
alsco77 <alsco77@pm.me>
2018-09-12 15:08:11 +1000

    Added deploy to gitlab ci script

--- functions.go
+++ functions.go
@@ -9,18 +9,6 @@ import (
 	"google.golang.org/appengine/log"
 )
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"os"
-
-// 	"cloud.google.com/go/firestore"
-// 	sendgrid "github.com/sendgrid/sendgrid-go"
-// 	"github.com/sendgrid/sendgrid-go/helpers/mail"
-// 	"google.golang.org/api/option"
-// 	"google.golang.org/appengine/log"
-// )
-
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
@@ -37,22 +25,3 @@ func writeLogIfError(ctx context.Context, err error) {
 		log.Errorf(ctx, "Err: %s", err.Error())
 	}
 }
-
-// func sendEmail() {
-// 	from := mail.NewEmail("Example User", "valthrex@gmail.com")
-// 	fmt.Println(from)
-// 	subject := "Sending with SendGrid is Fun"
-// 	to := mail.NewEmail("Example User", "valthrex@gmail.com")
-// 	plainTextContent := "and easy to do anywhere, even with Go"
-// 	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
-// 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-// 	client := sendgrid.NewSendClient(sendgridAPIKey)
-// 	response, err := client.Send(message)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	} else {
-// 		fmt.Println(response.StatusCode)
-// 		fmt.Println(response.Body)
-// 		fmt.Println(response.Headers)
-// 	}
-// }
